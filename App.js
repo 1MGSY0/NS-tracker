24e3f1dbc1754b518a7b801004a9e0847630fe21
1MGSY0 <1MGSY0@users.noreply.github.com>
2022-06-13 01:33:07 +0800

    tab bar

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { AntDesign } from '@expo/vector-icons'; 
+import { FontAwesome } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
+import HealthDataPage from './Screens/HealthDataPage';
+import AddHealthData from './Screens/AddHealthData';
+import ContactScreen from './Screens/ContactScreen';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            //Set the Icon based on which route it is
+            if (route.name === 'Health') {
+              return <FontAwesome name="heartbeat" size={size} color={color} />;
+            } else if (route.name === 'Add') {
+              return <Ionicons name="add" size={size} color={color} />;
+            } else if (route.name === 'Contact') {
+              return <AntDesign name="contacts" size={size} color={color} />;
+            }
+          },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+        })}
+      >
+        <Tab.Screen name="Health" component={HealthDataPage} />
+        <Tab.Screen name="Add" component={AddHealthData} />
+        <Tab.Screen name="Contact" component={ContactScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
